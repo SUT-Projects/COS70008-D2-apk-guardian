76dbb794a4c4ce9f1b1b6f9b921b5a8cc675dba5
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2025-05-22 16:44:39 +1000

    Create Few APIs in Prediction

--- backend/controllers/prediction_controller.py
+++ backend/controllers/prediction_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from datetime import datetime
 from os import path
 from services import prediction_service
@@ -6,8 +6,17 @@ from services import prediction_service
 prediction_route_bp = Blueprint("prediction", __name__, url_prefix="/prediction")
 
 prediction_route_bp.add_url_rule("/upload", view_func=prediction_service.predict, methods=["POST"])
+prediction_route_bp.add_url_rule("/", view_func=prediction_service.get_all_predictions, methods=["GET"])
 
-@prediction_route_bp.route("/", methods=["GET"])
-def home_page():
-    return {"message": "Hello Prediction", "date": datetime.now()}
+@prediction_route_bp.route("/<prediction_id>", methods=["GET"])
+def fetch_prediction(prediction_id):
+    doc, error = prediction_service.get_prediction_by_id(prediction_id)
 
+    if error == "invalid_format":
+        return jsonify({"error": "Invalid prediction ID format"}), 400
+
+    if error == "not_found":
+        return jsonify({"error": "Prediction not found"}), 404
+
+    # success!
+    return jsonify(doc), 200
